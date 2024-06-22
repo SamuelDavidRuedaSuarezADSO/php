aa496bd9549a38dd6ad8f7a2f19d753753f80305
SamuelDavidRuedaSuarezADSO <samueldavidruedasuarez3@gmail.com>
2024-06-22 11:56:52 -0500

    primerisima clase php

---
+++ index.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php echo "Demo PHP" ?></title>
+</head>
+
+<body>
+  <?php
+
+
+  // $numero = 20;
+  // $num = 10;
+
+  // $suma = $num + $numero;
+
+  // echo $suma;
+
+  // echo "<br>";
+
+  // $edad = 38;
+
+  // if ($suma < 17 && $suma > 0) {
+  //   echo "es menor de edad";
+  // } else {
+  //   echo "Es mayor de edad";
+  // }
+
+  // echo "<br>";
+
+  // $dia = 6;
+
+  // switch ($dia) {
+  //   case '1':
+  //     echo "lunes";
+  //     break;
+  //   case '2':
+  //     echo "martes";
+  //     break;
+  //   case '3':
+  //     echo "miercoles";
+  //     break;
+  //   case '4':
+  //     echo "jueves";
+  //     break;
+  //   case '5':
+  //     echo "viernes";
+  //     break;
+  //   case '6':
+  //     echo "sabado";
+  //     break;
+  //   case '7':
+  //     echo "domingo";
+  //     break;
+  //   default:
+  //     echo "Este dia es una mentira";
+  //     break;
+  // }
+
+  // echo "<br>";
+
+  // $res = sumar(5, 6);
+
+  // echo $res;
+
+  // function sumar($a, $b)
+  // {
+  //   return $a + $b;
+  // }
+
+  // $verdadero = false;
+  // $nombre = "Sam";
+  // $num1 = 3;
+
+  // var_dump($verdadero);
+  // echo "<br>";
+  // var_dump($nombre);
+  // echo "<br>";
+  // var_dump($num1);
+
+  // echo "<br>";
+
+  // $arreglo = [];
+  // $arreglo2 = [
+  //   'lunes',
+  //   'martes',
+  //   'miercoles',
+  //   'jueves',
+  //   'viernes',
+  //   "sabado",
+  //   "domingo",
+  // ];
+
+  // // array_push($arreglo, "sabado", "domingo");
+
+  // for ($i = 0; $i < 7; $i++) {
+  //   // array_push($arreglo, $arreglo2[$i]); 
+  //   switch ($i) {
+  //     case 0:
+  //       array_push($arreglo, 'lunes');
+  //       break;
+  //     case 1:
+  //       array_push($arreglo, 'martes');
+  //       break;
+  //     case 2:
+  //       array_push($arreglo, 'miercoles');
+  //       break;
+  //     case 3:
+  //       array_push($arreglo, 'jueves');
+  //       break;
+  //     case 4:
+  //       array_push($arreglo, 'viernes');
+  //       break;
+  //     case 5:
+  //       array_push($arreglo, 'sabado');
+  //       break;
+  //     case 6:
+  //       array_push($arreglo, 'domingo');
+  //       break;
+  //   }
+  // };
+  // var_dump($arreglo);
+  // echo "<br>";
+
+  include_once('aprendiz.php');
+  include_once('instructor.php');
+
+  // $sam = new Persona();
+  // //leno los compos
+  // $sam->setNombre('SAM');
+  // $sam->setApellido(('RUEDA'));
+  // $sam->setGenero(('MASCULINO'));
+  // $sam->setEdad((20));
+
+
+
+  // var_dump(($sam->getNombre()));
+  // var_dump(($sam->getApellido()));
+  // var_dump(($sam->getGenero()));
+  // var_dump(($sam->getEdad()));
+
+  // var_dump(($sam));
+
+  // echo "<br>";
+  // echo "<br>";
+
+
+  // var_dump(($sam->guardar()));
+
+  // // $dan = new Aprendiz();
+
+  // $dan->setNombre('Daniel');
+
+  // var_dump($dan)
+
+
+
+
+
+
+
+  ?>
+
+</body>
+
+</html>
